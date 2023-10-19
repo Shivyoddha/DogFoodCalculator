81eb83f60f1208630a1af41dc0ef55a11640b290
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2023-10-19 14:49:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 @st.cache
 def calculate_food(activity, weight):
     if activity and weight:
-        calculated_amount = activity / weight
+        calculated_amount = round(activity / weight,2)
         return {"recommendedFood": calculated_amount}
     else:
         return {"error": "Please provide both activity level and weight."}
